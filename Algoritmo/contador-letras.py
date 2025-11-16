bd706f56bf67362a39d5e26686798ea488bb45af
olivacristian706 <olivacristian706@hotmail.com>
2025-11-16 04:13:43 -0300

    Realizamos un contador de palabras en curso Digital House con algoritmos y pseudocódigo

---
+++ Algoritmo/contador-letras.py
@@ -0,0 +1,16 @@
+# --------------------------------------------------------------------
+# Algoritmo para contar la cantidad de letras en una palabra o frase
+# --------------------------------------------------------------------
+
+# Paso 1: solicitar entrada al usuario
+
+palabra_ingresada = input ("Por favor, ingresa una palabra o frase: ")
+    
+# Paso2: contar la cantidad de letras
+
+cantidad_letras = len(palabra_ingresada)
+    
+# Paso3: mostrar al usuario el resultado
+
+print("La palabra o frase tiene", cantidad_letras, "letras.")
+    
